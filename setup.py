586e1bc218aa59990ffd3c65cdb2d47625970a69
shrihan <dshrihan@gmail.com>
2016-11-06 17:03:15 -0500

    Hello from VCS

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SciFair-Project-Shrihan-and-Nikhil-',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='shrihan',
+    author_email='dshrihan@gmail.com',
+    description='This is our Science Fair Project test code use to test our hypothesis'
+)
