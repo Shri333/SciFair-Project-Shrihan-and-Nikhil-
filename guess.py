67846f8e813cb9a3d6b320af8c493dd383cdd90e
shrihan <dshrihan@gmail.com>
2016-11-06 14:43:43 -0500

    Hello from VCS

---
+++ guess.py
@@ -0,0 +1,21 @@
+import random
+
+def symbol1():
+    pass
+
+def symbol2():
+    pass
+
+def symbol3():
+    pass
+
+def symbol4():
+    pass
+
+def symbol5():
+    pass
+
+def symbol6():
+    pass
+
+symbol1()
