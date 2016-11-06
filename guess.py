7e43c6fe98018a398564e9cb944bc6bcdbb8d3ba
shrihan <dshrihan@gmail.com>
2016-11-06 14:31:14 -0500

    Hello from the other side

--- guess.py
+++ guess.py
@@ -1,1 +1,1 @@
-
+import symbol1
--- symbol1.py
+++ symbol1.py
@@ -1,13 +1,2 @@
-symbols = ["#", "$", "&"]
-
-password = input("Enter password (1 symbol): ")
-
-if len(password) == 1:
-    for i in range(0,len(password)):
-        for j in symbols:
-            if password[i] == j:
-                print("OK")
-            else:
-                print("BAD")
-else:
-    print("BAD")
+def main():
+    pass
