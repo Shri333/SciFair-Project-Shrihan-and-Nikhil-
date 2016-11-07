b72e07b1d3480c6332f7513f126da0713bb4326e
TheRealCell <dshrihan@gmail.com>
2016-11-06 19:16:35 -0500

    Update guess.py

--- guess.py
+++ guess.py
@@ -4,16 +4,26 @@ symbols = ["#", "$", "&"]
 
 def random_algorithm(length):
     pass1 = input("Enter password (" + str(length) + " symbols): ")
-    counter = 0
-    rand = ''.join(random.choice(symbols) for i in range(length))
-    print(rand)
-    while True:
-        if pass1 == rand:
-            print(str(counter) + " attempts")
-            break
-        else:
-            rand = ''.join(random.choice(symbols) for i in range(length))
-            print(rand)
-            counter += 1
-
-random_algorithm(3)
+    if len(pass1) != length:
+    	print("The password you entered is not the length specified")
+    else:
+    	found = True
+    	for i in range(length):
+    		if pass1[i] != symbols[0] and pass1[i] != symbols[1] and pass1[i] != symbols[2]:
+    			print("the password you entered has characters that do not equal the symbols listed above.")
+    			found = False
+    			break
+    	if found == True:	
+    		counter = 0
+    		rand = ''.join(random.choice(symbols) for i in range(length))
+    		print(rand)
+    		while True:
+    			if pass1 == rand:
+    				print(str(counter) + " attempts")
+    				break
+    			else:
+    				rand = ''.join(random.choice(symbols) for i in range(length))
+    				print(rand)
+    				counter += 1
+    				
+random_algorithm(4)
