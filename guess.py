5723932a4f48b83e6082665a56e8bf4a0c7eba57
TheRealCell <dshrihan@gmail.com>
2016-11-07 16:37:26 -0500

    Update guess.py

--- guess.py
+++ guess.py
@@ -5,28 +5,28 @@ symbols = ["#", "$", "&"]
 def random_algorithm(password, length):
 	counter = 0
 	rand = ''.join(random.choice(symbols) for i in range(length))
-	print(rand)
 	while True:
 		if password == rand:
+			print("Match found: " + rand)
 			print(str(counter) + " attempts")
 			break
 		else:
 			rand = ''.join(random.choice(symbols) for i in range(length))
-			print(rand)
 			counter += 1
-
+    		
 def execute_random_algorithm(length):
-    pass1 = input("Enter password (" + str(length) + " symbols): ")
-    if len(pass1) != length:
-    	print("The password you entered is not the length specified")
-    else:
-    	found = True
-    	for i in range(length):
-    		if pass1[i] != symbols[0] and pass1[i] != symbols[1] and pass1[i] != symbols[2]:
-    			print("the password you entered has characters that do not equal the symbols listed above.")
-    			found = False
-    			break
-    	if found == True:
-    		random_algorithm(pass1, length)
-    				
-execute_random_algorithm(4)
+	print("Choose from " + str(symbols))
+	password = input("Enter password (" + str(length) + " symbols): ")
+	if len(password) != length:
+		print("The password you entered is not the length specified")
+	else:
+		found = True
+		for i in range(length):
+			if password[i] != symbols[0] and password[i] != symbols[1] and password[i] != symbols[2]:
+				print("the password you entered has characters that do not equal the symbols listed above.")
+				found = False
+				break
+		if found == True:
+			random_algorithm(password, length)
+
+execute_random_algorithm(6)
