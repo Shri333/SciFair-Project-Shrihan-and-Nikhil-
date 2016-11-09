0d3f1303ab4081c0d7b74f1396eb1cb005b94830
shrihan <dshrihan@gmail.com>
2016-11-08 19:10:14 -0500

    Update guess.py

--- guess.py
+++ guess.py
@@ -2,9 +2,6 @@ import random
 
 symbols = ["#", "$", "&"]
 
-# Refactoring code:
-# Split methods into more methods to refactor code.
-
 # Random algorithm method that uses a generator to generate passwords.
 
 
@@ -17,24 +14,18 @@ def random_algorithm(password, length):
     while True:
         # If the password is equal to the generated password, then stop the loop and display the number of attempts.
         if password == rand:
-            # A match is found.
             print("Match found: " + rand)
-            # Display number of attempts.
             print(str(counter) + " attempts")
-            # Stop loop from continuing on.
             break
         # If the password is not equal to the generate password, then continue the loop and add an attempt (counter).
         else:
-            # Generate a new password with algorithm.
             rand = ''.join(random.choice(symbols) for _ in range(length))
-            # Increment number of attempts.
             counter += 1
 
 # Main method where the password is checked based on length and characters.
 
 
 def execute_random_algorithm(length):
-    # Display Choose from #, $, and &.
     print("Choose from " + str(symbols))
     # Take password input from the user.
     password = input("Enter password (" + str(length) + " symbols): ")
@@ -48,13 +39,10 @@ def execute_random_algorithm(length):
         for i in range(length):
             if password[i] != symbols[0] and password[i] != symbols[1] and password[i] != symbols[2]:
                 print("the password you entered has characters that do not equal the symbols listed above.")
-                # The password does not match the characters specified.
                 found = False
-                # Stop the loop and exit the program.
                 break
         # If the characters match then execute the random algorithm.
         if found:
-            # Execute the random algorithm.
             random_algorithm(password, length)
 
 # Execute the main program with the length specified.
